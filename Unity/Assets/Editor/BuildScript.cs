ab15717d719a7a6bfae45e5da0c5874c18cefb90
Eric Fruchter <fruchter.eric@gmail.com>
2018-09-03 11:59:40 -0700

    build script

---
+++ Unity/Assets/Editor/BuildScript.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+
+class BuildScript
+{
+    static string[] SCENES = FindEnabledEditorScenes();
+
+    static string APP_NAME = "YourProject";
+    static string TARGET_DIR = "target";
+
+    private static string[] FindEnabledEditorScenes()
+    {
+        List<string> EditorScenes = new List<string>();
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if (!scene.enabled) continue;
+            EditorScenes.Add(scene.path);
+        }
+        return EditorScenes.ToArray();
+    }
+
+    [MenuItem("Custom/CI/Win64")]
+    static void PerformWIn64Build()
+    {
+        string target_dir = APP_NAME + ".exe";
+        GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.StandaloneWindows64, BuildOptions.None);
+    }
+
+    static void GenericBuild(string[] scenes, string target_dir, BuildTarget build_target, BuildOptions build_options)
+    {
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Standalone, BuildTarget.StandaloneWindows64);
+        var res = BuildPipeline.BuildPlayer(scenes, target_dir, build_target, build_options);
+        if (res.summary.result != UnityEditor.Build.Reporting.BuildResult.Succeeded)
+        {
+            throw new Exception("BuildPlayer failure: " + res);
+        }
+    }
+}
