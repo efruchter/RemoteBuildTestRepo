024499981933a73fbc114eeea7c7312db3255023
Eric Fruchter <fruchter.eric@gmail.com>
2018-09-03 11:32:40 -0700

    sample scene that is lit af

---
+++ Unity/Assets/Controls.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Hello Jenkins!");
+    }
+
+    void Update ()
+    {
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+	}
+}
---
+++ Unity/Assets/SpinAround.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinAround : MonoBehaviour
+{
+	void Update ()
+    {
+	    transform.rotation *= Quaternion.Euler(Time.deltaTime * 90f / 2, Time.deltaTime * 90f / 3, Time.deltaTime * 90f / 4f);
+	}
+}
